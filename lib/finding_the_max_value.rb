2f6c2b2a673e929aa8b6fb74532987c7dbe3ebeb
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-03 22:46:09 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,9 @@
 def find_max_value(array)
-  # Add your solution here
+  max_value = 0 
+  array.length.times do |index|
+    if array[index] > max_value
+      max_value = array[index]
+    end 
+  end 
+  return max_value 
 end
