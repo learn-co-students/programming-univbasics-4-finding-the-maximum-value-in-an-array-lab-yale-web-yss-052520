64541bb99a052669632c33c492bc0aeaeeef1665
James B Schoen <edensongjbs@gmail.com>
2020-05-06 16:28:38 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,10 @@
 def find_max_value(array)
   # Add your solution here
+  current_highest=0
+  array.each { |i| 
+    if i>current_highest
+      current_highest=i
+    end
+  }
+  current_highest
 end
